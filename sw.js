9c2e5453fd2ad6a29d1644a21bbad58f379b1b94
Jignesh Shah <63937000+jssshah@users.noreply.github.com>
2026-09-24 02:38:07 -0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// Keeps BP Record working with no internet once it has been opened once.
-const C="vitality-bp-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"])).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok&&new URL(e.request.url).origin===location.origin){const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp))}return res}).catch(()=>caches.match("./index.html"))))});
